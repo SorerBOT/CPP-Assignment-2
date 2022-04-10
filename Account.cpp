20f86b82c254f7e0e276613f7c2f91d5d13754e9
SorerBOT <alonfiller@gmail.com>
2022-04-10 12:37:54 +0300

    (Feat) Implemented blank Account constructor

--- Account.cpp
+++ Account.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Account.h"
+
+Account::Account() {
+    this->m_transactionList = NULL;
+    this->m_numberOfTransaction = 0;
+    this->m_persons = NULL;
+    this->m_totalPersons = NULL;
+    this->m_accountNumber = NULL;
+    this->m_balance = NULL;
+}
