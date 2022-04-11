0d3b51ecd047c75c0c93e8acc0b7de7be50e754e
SorerBOT <alonfiller@gmail.com>
2022-04-11 10:23:45 +0300

    (Feat) Implemented default (Full) Transaction comstructor.

--- Transaction.cpp
+++ Transaction.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "Transaction.h"
+// Initalised m_date to NULL in order to avoid: Clang-Tidy: Constructor does not initialize these fields: m_date
+Transaction::Transaction(Account *s, Account *d, double amount, const char *date): m_date(NULL) {
+    this->m_source = s;
+    this->m_destination = d;
+    this->m_amount = amount;
+    strcpy(this->m_date, date);
+}
