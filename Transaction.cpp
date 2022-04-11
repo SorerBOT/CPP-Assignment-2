3bbd304b62c8afac5aab0a18fbfe4a9a7902e5c6
SorerBOT <alonfiller@gmail.com>
2022-04-11 10:31:21 +0300

    (Feat) Implemented Transaction setter functions.

--- Transaction.cpp
+++ Transaction.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "Transaction.h"
-// Initalised m_date to NULL in order to avoid: Clang-Tidy: Constructor does not initialize these fields: m_date
+// Initialised m_date to NULL in order to avoid: Clang-Tidy: Constructor does not initialize these fields: m_date
 Transaction::Transaction(Account *s, Account *d, double amount, const char *date): m_date(NULL) {
     this->m_source = s;
     this->m_destination = d;
@@ -15,4 +15,17 @@ Transaction::Transaction(const Transaction &other): m_date(NULL){
     this->m_destination = other.m_destination;
     this->m_amount = other.m_amount;
     strcpy(this->m_date, other.m_date);
+}
+
+void Transaction::SetAmount(double amount) {
+    this->m_amount = amount;
+}
+void Transaction::SetDate(const char* date) {
+    strcpy(this->m_date, date);
+}
+void Transaction::SetDes(Account* dst) {
+    this->m_destination = dst;
+}
+void Transaction::SetSource(Account* src) {
+    this->m_source = src;
 }
