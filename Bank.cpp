4714447b411ff3dad436afd50bce1109d7a2aa18
SorerBOT <alonfiller@gmail.com>
2022-04-12 10:50:08 +0300

    (Feat) Implemented function Bank::SetAccount

--- Bank.cpp
+++ Bank.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "Bank.h"
+
+Bank::Bank() {
+    this->m_account = NULL;
+    this->m_bankCode = 0;
+    this->m_name = NULL;
+    this->m_numbeOfAccounts = 0;
+    this->m_totalBalance = 0;
+}
+Bank::Bank(const char *name, int code) {
+    this->m_account = NULL;
+    this->m_numbeOfAccounts = 0;
+    this->m_totalBalance = 0;
+    this->m_bankCode = code;
+    strcpy(this->m_name, name);
+}
+Bank::~Bank() {
+    int iteration;
+    for (iteration = 0; iteration < this->m_numbeOfAccounts; iteration++) delete this->m_account[iteration];
+    delete[] this->m_account;
+}
+
+void Bank::SetAccount(Account **account, int numbeOfAccounts) {
+    int iteration;
+    for (iteration = 0; iteration < this->m_numbeOfAccounts; iteration++) delete this->m_account[iteration];
+    delete[] this->m_account;
+
+    this->m_numbeOfAccounts = numbeOfAccounts;
+    this->m_account = new Account*[this->m_numbeOfAccounts];
+
+    for (iteration = 0; iteration < this->m_numbeOfAccounts; iteration++) this->m_account[iteration] = new Account(*account[iteration]);
+
+    for (iteration = 0; iteration < numbeOfAccounts; iteration++) delete account[iteration];
+    delete[] account;
+}
